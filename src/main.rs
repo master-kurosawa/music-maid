9f496fa599ba1092b32bf9d71519100c892b6ecb
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-10-18 12:18:36 +0200

    :zap: faster than lightning

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,42 @@
-use ignore::Walk;
-use std::{error::Error, path::Path};
+use ignore::{WalkBuilder, WalkState};
+use std::{
+    error::Error,
+    path::{Path, PathBuf},
+    sync::{Arc, Mutex},
+};
 use tokio_uring::fs::File;
 
-async fn read_with_uring(path: &Path) -> Result<(), Box<dyn Error>> {
+async fn read_with_uring(path: &Path) -> Result<(), Box<dyn Error + Send + Sync>> {
     let file = File::open(path).await?;
     let buf = vec![0; 4096];
     let (_res, _buf) = file.read_at(buf, 0).await;
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    tokio_uring::start(async {
-        let mut tasks = Vec::new();
+fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let paths: Arc<Mutex<Vec<Arc<PathBuf>>>> = Arc::new(Mutex::new(Vec::new()));
+    let mut tasks = Vec::new();
+    let builder = WalkBuilder::new("./tmp");
+    builder.build_parallel().run(|| {
+        Box::new(|path| {
+            match path {
+                Ok(entry) => {
+                    let path = Arc::new(entry.path().to_path_buf());
+                    let clone_xd = Arc::clone(&paths);
+                    clone_xd.lock().unwrap().push(path);
+                }
+                Err(_) => panic!(),
+            }
 
-        for entry in Walk::new("../") {
-            let path = entry.unwrap().path().to_path_buf();
-            let spawn = tokio_uring::spawn(async move { read_with_uring(&path).await });
-            tasks.push(spawn);
+            WalkState::Continue
+        })
+    });
+    tokio_uring::start(async {
+        for entry in paths.lock().into_iter() {
+            entry.clone().into_iter().for_each(|path| {
+                let spawn = tokio_uring::spawn(async move { read_with_uring(&path).await });
+                tasks.push(spawn);
+            });
         }
 
         for task in tasks {
