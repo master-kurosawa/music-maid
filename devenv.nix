022eb6382baa35517e2dd5c1ff436a7d430c6b1c
master-kurosawa <onanie.master.kurosawa@protonmail.com>
2024-10-25 19:19:37 +0200

    ❄️ Add mbslave & setup postgres for local dev

--- devenv.nix
+++ devenv.nix
@@ -5,8 +5,27 @@
   inputs,
   ...
 }: {
+  packages = [
+    inputs.mbslave.packages.${pkgs.system}.default
+    pkgs.pgadmin4-desktopmode
+    pkgs.sqlx-cli
+  ];
+
   languages.rust = {
     enable = true;
     channel = "nightly";
   };
+
+  # for mbslave
+  services.postgres = {
+    enable = true;
+    initialScript = ''
+      CREATE ROLE postgres WITH LOGIN SUPERUSER PASSWORD 'postgres';
+    '';
+  };
+  dotenv.enable = true;
+
+  enterShell = ''
+    export DATABASE_URL=postgres:///musicbrainz?host=$PGHOST
+  '';
 }
